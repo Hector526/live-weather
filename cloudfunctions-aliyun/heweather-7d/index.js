19f11951fb6e7831e93f1ee351236393abf373b0
Hector <wolfares@163.com>
2020-07-17 15:34:10 +0800

    新增24小时，7天和生活指数云函数

---
+++ cloudfunctions-aliyun/heweather-7d/index.js
@@ -0,0 +1,19 @@
+'use strict';
+const HEWEATHER_KEY = 'c7266e3923a84a718c2e560993cc483c';
+const API_URL = 'https://devapi.heweather.net/v7/weather/7d';
+
+exports.main = async (event, context) => {
+	const {lat, lon} = event;
+	let location = `${lon},${lat}`;
+
+	const options = {
+		method: 'GET',
+		data: {
+			location: location,
+			key: HEWEATHER_KEY,
+		},
+		dataType: 'json'
+	};
+
+	return await uniCloud.httpclient.request(API_URL, options);
+}
--- libs/api.js
+++ libs/api.js
@@ -4,13 +4,13 @@
  * @param {*} lon
  */
 export const geocoder = (lat, lon) => {
-  return uniCloud.callFunction({
-    name: 'tencent-geocoder',
-    data: {
-      lat,
-      lon
-    }
-  })
+	return uniCloud.callFunction({
+		name: 'tencent-geocoder',
+		data: {
+			lat,
+			lon
+		}
+	})
 }
 
 /**
@@ -19,13 +19,13 @@ export const geocoder = (lat, lon) => {
  * @param {*} lon
  */
 export const heweatherNow = (lat, lon) => {
-  return uniCloud.callFunction({
-    name: 'heweather-now',
-    data: {
-      lat,
-      lon
-    }
-  })
+	return uniCloud.callFunction({
+		name: 'heweather-now',
+		data: {
+			lat,
+			lon
+		}
+	})
 }
 
 /**
@@ -34,26 +34,71 @@ export const heweatherNow = (lat, lon) => {
  * @param {*} lon
  */
 export const heweatherAir = (lat, lon) => {
-  return uniCloud.callFunction({
-    name: 'heweather-air',
-    data: {
-      lat,
-      lon
-    }
-  })
+	return uniCloud.callFunction({
+		name: 'heweather-air',
+		data: {
+			lat,
+			lon
+		}
+	})
 }
 
 /**
- * 获取和风天气质量
+ * 获取和风天气3天预报
  * @param {*} lat
  * @param {*} lon
  */
 export const heweather3d = (lat, lon) => {
-  return uniCloud.callFunction({
-    name: 'heweather-3d',
-    data: {
-      lat,
-      lon
-    }
-  })
+	return uniCloud.callFunction({
+		name: 'heweather-3d',
+		data: {
+			lat,
+			lon
+		}
+	})
+}
+
+/**
+ * 获取和风天气逐小时预报（未来24小时）
+ * @param {*} lat
+ * @param {*} lon
+ */
+export const heweather24h = (lat, lon) => {
+	return uniCloud.callFunction({
+		name: 'heweather-24h',
+		data: {
+			lat,
+			lon
+		}
+	})
+}
+
+/**
+ * 获取和风天气7天预报
+ * @param {*} lat
+ * @param {*} lon
+ */
+export const heweather7d = (lat, lon) => {
+	return uniCloud.callFunction({
+		name: 'heweather-7d',
+		data: {
+			lat,
+			lon
+		}
+	})
+}
+
+/**
+ * 获取和风天气当天生活指数预报
+ * @param {*} lat
+ * @param {*} lon
+ */
+export const heweatherIndices = (lat, lon) => {
+	return uniCloud.callFunction({
+		name: 'heweather-indices',
+		data: {
+			lat,
+			lon
+		}
+	})
 }
